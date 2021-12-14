f2c1ef88fbbf8afc8b1299690d66392381909b1a
MWayneMercado <MWayneMercado@gmail.com>
2021-12-14 15:51:11 -0800

    remove user route and add note endpoint

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
