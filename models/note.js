320e22c49aa34c880a4377062c92d99d1fbdbffe
MWayneMercado <MWayneMercado@gmail.com>
2021-12-15 14:49:03 -0800

    add series value to noteSchema

--- models/note.js
+++ models/note.js
@@ -7,6 +7,7 @@ const Schema = mongoose.Schema;
 // ADD SERIES TO NOTE SCHEMA
 
 let noteSchema = new Schema({
+  series: {Type: Schema.Types.ObjectId, ref: 'Series', required: true},
   title: {type: String, required: true},
   location: {type: String, required: true},
   synposis: {type: String, required: true},
