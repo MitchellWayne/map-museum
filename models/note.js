551622d5bb8a0880ffbb146f705dc657acde852e
MWayneMercado <MWayneMercado@gmail.com>
2021-12-14 16:09:05 -0800

    define note model

---
+++ models/note.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// TODO
+// ADD SERIES MODEL
+// SERIES DOCUMENTS CONTAINS LIST OF NOTES
+// ADD SERIES TO NOTE SCHEMA
+
+let noteSchema = new Schema({
+  title: {type: String, required: true},
+  location: {type: String, required: true},
+  synposis: {type: String, required: true},
+  locdetails: {type: String, required: true},
+  longitude: {type: Double, required: true},
+  latitude: {type: Double, required: true},
+  image: {type: String, required: true},
+});
+
+module.exports = mongoose.model('Note', noteSchema);
