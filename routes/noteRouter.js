54c6f358bbdf692e025ef92e83fc0398abe046a9
MWayneMercado <MWayneMercado@gmail.com>
2021-12-16 12:54:04 -0800

    change postID to noteID

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -7,9 +7,9 @@ const noteController = require('../controllers/noteController.js');
 // This should probably only return some data (Like just IDs + Title + Coords / etc.)
 router.get('/', noteController.notelist_get);
 
-router.get('/:postID', noteController.note_get);
+router.get('/:noteID', noteController.note_get);
 router.post('/', noteController.note_post);
-router.put('/:postID', noteController.note_put);
-router.delete('/:postID', noteController.note_delete);
+router.put('/:noteID', noteController.note_put);
+router.delete('/:noteID', noteController.note_delete);
 
 module.exports = router;
