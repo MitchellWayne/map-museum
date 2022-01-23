3f15946bab75264f167ddb4a6e32778bd15185d3
MWayneMercado <MWayneMercado@gmail.com>
2022-01-23 11:58:43 -0800

    add series_post route

--- routes/seriesRouter.js
+++ routes/seriesRouter.js
@@ -5,4 +5,6 @@ const seriesController = require('../controllers/seriesController.js');
 
 series.get('/', seriesController.serieslist_get);
 
+series.post('/', seriesController.series_post);
+
 module.exports = router;
