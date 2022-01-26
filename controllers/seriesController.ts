94b9ec781f148028c9c8279a9073d165abfe7167
MWayneMercado <MWayneMercado@gmail.com>
2022-01-25 16:44:52 -0800

    define serieslist_get

--- controllers/seriesController.ts
+++ controllers/seriesController.ts
@@ -3,7 +3,16 @@ export {};
 const Series = require('../models/series');
 
 exports.serieslist_get = function(req, res) {
-  const filter = req.query;
+  const seriesfilter = req.query;
 
-  
+  Series.find()
+  .select('name notes')
+  .exec(function(err, serieslist){
+    if(err) return res.status(400).json(err);
+    else if (seriesfilter) { 
+      serieslist = serieslist.filter(series => (series.name.toLowerCase()).includes(seriesfilter));
+      return res.status(200).json(serieslist);
+    }
+    else return res.status(200).json(serieslist);
+  });
 };
