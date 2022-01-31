e587946b5724c45c7e2492fb6cb883922f429366
MWayneMercado <MWayneMercado@gmail.com>
2022-01-31 14:29:48 -0800

    define s3 uploadFile middleware

---
+++ src/s3.ts
@@ -0,0 +1,26 @@
+require('dotenv').config();
+import fs from 'fs';
+import S3, { PutObjectRequest } from 'aws-sdk/clients/s3';
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+export function uploadFile(file: Express.Multer.File) {
+  const fileStream = fs.createReadStream(file.path);
+
+  const uploadParams: PutObjectRequest = {
+    Bucket: bucketName as string,
+    Body: fileStream,
+    Key: file.filename,
+  };
+
+  return s3.upload(uploadParams).promise();
+}
