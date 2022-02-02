88862e5010db8638d04198fb6031f5459e4c0514
MWayneMercado <MWayneMercado@gmail.com>
2022-02-02 11:26:31 -0800

    revert previous commit

--- src/models/note.ts
+++ src/models/note.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
-  series: { type: Schema.Types.ObjectId, ref: 'Series', required: false },
+  series: { type: Schema.Types.ObjectId, ref: 'Series', required: true },
   title: { type: String, required: true },
   location: { type: String, required: true },
   synopsis: { type: String, required: true },
