21a8c33c9556dce0e5316cc21b0df825c041f1d0
MWayneMercado <MWayneMercado@gmail.com>
2022-02-02 13:24:50 -0800

    remove 'required' option on nodes ID array

--- src/models/series.ts
+++ src/models/series.ts
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const seriesSchema = new Schema({
   name: { type: String, required: true },
-  notes: [{ type: Schema.Types.ObjectId, ref: 'Note', required: true }],
+  notes: [{ type: Schema.Types.ObjectId, ref: 'Note', required: false }],
 });
 
 // module.exports = mongoose.model('Series', seriesSchema);
