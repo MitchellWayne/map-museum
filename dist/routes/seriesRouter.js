243991167d86f8d8b5fd0bbbd617cfae8b58f6eb
MWayneMercado <MWayneMercado@gmail.com>
2022-02-01 16:17:51 -0800

    add checkseries middleware to series_post

--- dist/routes/seriesRouter.js
+++ dist/routes/seriesRouter.js
@@ -24,7 +24,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+const validators_1 = require("../validators");
 const seriesController = __importStar(require("../controllers/seriesController.js"));
 router.get('/', seriesController.serieslist_get);
-router.post('/', seriesController.series_post);
+router.post('/', (0, validators_1.checkSeries)(), seriesController.series_post);
 exports.default = { router };
--- src/routes/seriesRouter.ts
+++ src/routes/seriesRouter.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
+import { checkSeries } from '../validators';
+
 import * as seriesController from '../controllers/seriesController.js';
 
 router.get('/', seriesController.serieslist_get);
 
-router.post('/', seriesController.series_post);
+router.post('/', checkSeries(), seriesController.series_post);
 
 export default { router };
