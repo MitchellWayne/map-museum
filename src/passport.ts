9f41b07f17f8ed032f209536764c115ca7b26444
MWayneMercado <MWayneMercado@gmail.com>
2022-02-03 15:58:27 -0800

    define JWT strategy

--- src/passport.ts
+++ src/passport.ts
@@ -7,4 +7,24 @@ const ExtractJWT = passportJWT.ExtractJwt;
 
 require('dotenv').config();
 
-
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      // jwtFromRequest: req => req.cookies.token,
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    function (jwtPayload, callback) {
+      if (jwtPayload.user_id == process.env.USER_ID)
+        return callback(null, true);
+      else return callback(null, false);
+      // return User.findOne({ _id: jwtPayload.user_id })
+      //   .then((user) => {
+      //     return callback(null, user);
+      //   })
+      //   .catch((err) => {
+      //     return callback(err);
+      //   });
+    }
+  )
+);
