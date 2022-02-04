e02c15202cc645a13a93e7d132bc2362420c622d
MWayneMercado <MWayneMercado@gmail.com>
2022-02-03 16:05:21 -0800

    admin_controller function declarations

---
+++ src/controllers/adminController.ts
@@ -0,0 +1,9 @@
+import * as express from 'express';
+
+export function login_post(req: express.Request, res: express.Response) {
+  console.log('hey');
+}
+
+export function logout_post(req: express.Request, res: express.Response) {
+  console.log('hey');
+}
