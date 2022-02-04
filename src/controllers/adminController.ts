a2718e7f057f0ecb7b00f5ddb0be8c376b1a6a1d
MWayneMercado <MWayneMercado@gmail.com>
2022-02-03 17:36:07 -0800

    define adminController methods (login)

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,9 +1,35 @@
 import * as express from 'express';
+import jwt from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs';
 
-export function login_post(req: express.Request, res: express.Response) {
-  console.log('hey');
+require('dotenv').config();
+
+async function checkLogin(
+  username: string,
+  password: string
+): Promise<boolean> {
+  let valid = false;
+  await bcryptjs
+    .compare(password, process.env.USER_HASHED_PASSWORD as string)
+    .then((result) => {
+      if (result && username == process.env.USERNAME) {
+        valid = true;
+      }
+      return valid;
+    });
+  return valid;
 }
 
-export function logout_post(req: express.Request, res: express.Response) {
-  console.log('hey');
+export async function login_post(req: express.Request, res: express.Response) {
+  const success = await checkLogin(req.body.username, req.body.password);
+  if (success) {
+    const token = jwt.sign(
+      { userID: process.env.USER_ID },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '12h' }
+    );
+    return res.status(200).json({ token });
+  } else {
+    return res.status(400).json({ message: 'Username or Password incorrect' });
+  }
 }
