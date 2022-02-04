afa20e06ad7f5ad6e99dc851adce9874e7b43f3c
MWayneMercado <MWayneMercado@gmail.com>
2022-02-03 17:36:17 -0800

    passport cleanup

--- src/passport.ts
+++ src/passport.ts
@@ -1,6 +1,5 @@
 import passport from 'passport';
 import passportJWT from 'passport-jwt';
-import bcryptjs from 'bcryptjs';
 
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
@@ -11,20 +10,11 @@ passport.use(
   new JWTStrategy(
     {
       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      // jwtFromRequest: req => req.cookies.token,
       secretOrKey: process.env.JWT_SECRET,
     },
     function (jwtPayload, callback) {
-      if (jwtPayload.user_id == process.env.USER_ID)
-        return callback(null, true);
+      if (jwtPayload.userID == process.env.USER_ID) return callback(null, true);
       else return callback(null, false);
-      // return User.findOne({ _id: jwtPayload.user_id })
-      //   .then((user) => {
-      //     return callback(null, user);
-      //   })
-      //   .catch((err) => {
-      //     return callback(err);
-      //   });
     }
   )
 );
